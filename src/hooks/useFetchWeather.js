338e063e7c2346e874b032c4e9be360fd9cb7236
nonamed <druizr07@gmail.com>
2023-01-15 22:36:54 -0500

    Add tweaks to the useFetchWeather hook

--- src/hooks/useFetchWeather.js
+++ src/hooks/useFetchWeather.js
@@ -1,19 +1,19 @@
 import { useEffect, useState } from 'react';
-import { getWeather } from '../helpers/getWeather';
+import { getFormattedCurrentWeather } from '../services/getWeather';
 
 export const useFetchWeather = (city) => {
   const [weather, setWeather] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
 
   const getWeatherData = async () => {
-    const weatherData = await getWeather(city);
+    const weatherData = await getFormattedCurrentWeather({ q: city });
     setWeather(weatherData);
     setIsLoading(false);
   };
 
   useEffect(() => {
     getWeatherData();
-  }, []);
+  }, [city]);
 
   return { weather, isLoading };
 };
