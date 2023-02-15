2b3245fc46dabb7a6122062ee09c8612129f4873
nonamed <druizr07@gmail.com>
2023-02-14 20:16:31 -0500

    Add TailwindCSS

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { WeatherApp } from './WeatherApp'
 import '@fontsource/inter'
-import './global.css'
+// import './global.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
