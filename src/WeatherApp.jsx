2007f31a9dd4cb8514bc47d13581b07ab1d3303c
nonamed <druizr07@gmail.com>
2023-02-14 20:15:57 -0500

    Add mocks from APIs

--- src/WeatherApp.jsx
+++ src/WeatherApp.jsx
@@ -2,12 +2,31 @@ import { Form } from './components/Form/Form'
 import { Header } from './components/Header/Header'
 import { Container } from './components/Container/Container'
 import { Weather } from './components/Weather/Weather'
-import { useWeather } from './hooks/useWeather'
-import { useGeolocation } from './hooks/useGeolocation'
+// import { useWeather } from './hooks/useWeather'
+// import { useGeolocation } from './hooks/useGeolocation'
+// import geolocationResults from './mocks/geolocationAPI.json'
+import { weatherData } from './utils/weather'
+import { useEffect, useState } from 'react'
 
 export const WeatherApp = () => {
-  const { initialWeather, isLoading } = useGeolocation()
-  const { city } = useWeather({ initialWeather })
+  const [weather, setWeather] = useState({})
+  // const { initialWeather, isLoading } = useGeolocation()
+  // const { city } = useWeather({ initialWeather })
+  // console.log(openWeatherResults)
+
+  // if ('geolocation' in navigator) {
+  //   console.log(
+  //     navigator.geolocation.getCurrentPosition((pos) => {
+  //       const lat = pos.coords.latitude
+  //       const lon = pos.coords.longitude
+  //       console.log(lat, lon)
+  //     })
+  //   )
+  // }
+  useEffect(() => {
+    const weather = weatherData()
+    setWeather(weather)
+  }, [])
 
   return (
     <>
@@ -15,10 +34,10 @@ export const WeatherApp = () => {
       <Container>
         <Form />
         {/* <p>Search a city</p> */}
-        {isLoading && <h1>Loading...</h1>}
+        {/* {isLoading && <h1>Loading...</h1>} */}
         <Weather
-          weather={initialWeather}
-          isLoading={isLoading}
+          weather={weather}
+          // isLoading={isLoading}
           // units={units}
           // changeUnits={changeMetricSystem}
           // isError={isError}
